9986e4a6c9b302e0a259ab4f7d39cff81649f462
caduestrela <eduardostrelainfo@gmail.com>
2026-04-12 08:45:52 -0300

    ATULIZAÇÃO STRUCT

--- tj.h
+++ tj.h
@@ -3,22 +3,22 @@
 
 //Struct para variaveis dos calculos.
 typedef struct {
-    int suspm4_b = 0;
-    int julgm4_b = 0;
-    int distm4_b = 0;
-    int suspm4_a = 0;
-    int julgm4_a = 0;
-    int distm4_a = 0;
-    int suspm2_ant = 0;
-    int julgm2_ant = 0;
-    int distm2_ant = 0;
-    int suspm2_a = 0;
-    int julgm2_a = 0;
-    int distm2_a = 0;
-    int dessobrestados_2026 = 0;
-    int suspensos_2026 = 0;
-    int julgados_2026 = 0;
-    int casos_novos_2026 = 0;
+    int suspm4_b;
+    int julgm4_b;
+    int distm4_b;
+    int suspm4_a;
+    int julgm4_a;
+    int distm4_a;
+    int suspm2_ant;
+    int julgm2_ant;
+    int distm2_ant;
+    int suspm2_a;
+    int julgm2_a;
+    int distm2_a;
+    int dessobrestados_2026;
+    int suspensos_2026;
+    int julgados_2026;
+    int casos_novos_2026;
 
 } Calculos;
 
